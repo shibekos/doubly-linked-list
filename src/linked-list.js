9e5804e675071e7801d09813d5fec0d00c27e503
N. Shibeko <nikit.shibeko@gmail.com>
2017-01-27 22:40:15 +0300

    new

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,93 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+      this.length = 0;
+      this._tail = null;
+      this._head = null;
+      this.nodes = [];
+    }
 
-    append(data) {}
+    append(data) {
+     var node = new Node(data);
 
-    head() {}
+    if (this._length) {
+        this.tail.next = node;
+        node.previous = this.tail;
+        this.tail = node;
+    } else {
+        this.head = node;
+        this.tail = node;
+    }
 
-    tail() {}
+    this._length++;
 
-    at(index) {}
+    return node;
+   };
 
-    insertAt(index, data) {}
+    head() {
+      return data._head;
+    }
 
-    isEmpty() {}
+    tail(){
+      return data._tail;
+    }
 
-    clear() {}
+    at(index) {
+      return this.nodes.data;
+    }
 
-    deleteAt(index) {}
+    insertAt(index, data) {
+       var node = new Node(data);
+       if(this.nodes[index-1])this.nodes[index-1].appendChild(node);
+       this.nodes.splice(index,0,node);
+       this.length++;
+       return this;
+    }
 
-    reverse() {}
+    isEmpty() {
+      return this.length == 0;
+    }
 
-    indexOf(data) {}
+    clear() {
+      this.length = 0;
+      this.nodes = [];
+      this._head = null;
+      this._tail = null;
+      return this;
+    }
+
+    deleteAt(index) {
+      this.nodes[index].remove();
+      return this;
+    }
+
+    reverse(array) {
+      var next = null;
+      var prev = null;
+      var length = array.length;
+      for (next = 0; next < length / 2; next += 1)
+    {
+      prev = length - 1 - next;
+      var temporary = array[prev];
+       array[next] = array[prev];
+       array[prev] = temporary;
+    }
+      return array;
+    }
+
+    indexOf(data) {
+      if(this.length == 0) return;
+      var indexOfFindedNode = -1;
+      var searchedNode;
+      this.nodes.forEach(function(item){
+         if(item.data === data){
+            searchedNode = item;
+         }
+      });
+      indexOfFindedNode = this.nodes.indexOf(searchedNode);
+      return indexOfFindedNode;
+    }
 }
 
 module.exports = LinkedList;
